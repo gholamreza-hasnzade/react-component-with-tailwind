ed9af7b9916de6925c03e1563065274a1fd86f73
g.hassanzadeh <g.hassanzadeh@isiran.ir>
2025-08-26 11:53:59 +0330

    remove driver

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { SkeletonExample } from "./components/atoms/skeleton/skeleton.example";
-import { ToastContainer } from "./components/atoms/toast/ToastContainer";
+/* import { SkeletonExample } from "./components/atoms/skeleton/skeleton.example";
+ */import { ToastContainer } from "./components/atoms/toast/ToastContainer";
 
 function App() {
   return (
     <>
       <div className="w-full h-screen bg-gray-50 pb-60">
-        <SkeletonExample />
       </div>
       <ToastContainer position="top-right" maxToasts={5} />
     </>
