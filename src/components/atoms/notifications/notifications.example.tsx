3594860f92d81b4d6c60520c61303d010a97a25a
g.hassanzadeh <g.hassanzadeh@isiran.ir>
2025-08-27 11:22:31 +0330

    feat Notifications

--- src/components/atoms/notifications/notifications.example.tsx
+++ src/components/atoms/notifications/notifications.example.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { NotificationSystem } from "./notifications";
 import { useNotifications } from "./useNotifications";
 
--- src/components/atoms/notifications/notifications.tsx
+++ src/components/atoms/notifications/notifications.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { cn } from "../../../lib/utils";
+import { cn } from "@/lib/utils";
 import { 
   FaCheck, 
   FaExclamationTriangle, 
